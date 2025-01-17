419588ae0004b45e46e053e526d2f22c97f2b351
JAEBEOM AN <157469847+JBHAYDEN@users.noreply.github.com>
2025-01-17 16:21:26 +0900

    Merge Refactor
    import SinupScreen

--- lib/login.dart
+++ lib/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'forgot_password_screen.dart';
+import 'signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -164,7 +165,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => const ForgotPasswordScreen(),
+                          builder: (context) => const SignupScreen(),
                         ),
                       );
                     },
